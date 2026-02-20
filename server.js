c2987ea3a5c16a5740a41c1a678056419031314e
grayt9 <grayt9@xavier.edu>
2026-02-20 09:58:47 -0500

    changed db.createconnection to db.createpool to allow for reconnection and multiple connections

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ console.log("MYSQLPORT:", process.env.MYSQLPORT);
 
 
 
-
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -19,13 +18,17 @@ app.use(express.json());
 
 /* ---------- DATABASE CONNECTION ---------- */
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    port: process.env.MYSQLPORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
 
 
 db.connect((err) => {
